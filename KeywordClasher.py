9e38bfad2e2a75ec9accf8913481beea9a182f65
DanielLiddle <48800605+DanielLiddle@users.noreply.github.com>
2023-06-16 15:03:47 +0100

    Update KeywordClasher.py

--- KeywordClasher.py
+++ KeywordClasher.py
@@ -40,6 +40,11 @@ def find_pages_to_merge(data, position_threshold):
 
 st.title('SEO Pages Merger Tool')
 
+st.markdown('## Instructions')
+st.markdown('- Upload a CSV file containing your SEO data with the columns: Query, Landing Page, Impressions, Url Clicks, Average Position')
+st.markdown('- Set the minimum impressions and average position threshold as required.')
+st.markdown('- Click on the "Run" button to process the data.')
+
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
 min_impressions = st.number_input('Enter Minimum Impressions:', min_value=1, value=500)
@@ -48,7 +53,10 @@ position_threshold = st.number_input('Enter Average Position Threshold:', min_va
 
 if st.button('Run'):
     if uploaded_file is not None:
+        st.markdown('### Uploaded Data')
         data = pd.read_csv(uploaded_file)
+        st.dataframe(data)
+        
         filtered_data = data[data['Impressions'] >= min_impressions]
 
         grouped_data = filtered_data.groupby(['Query', 'Landing Page']).agg({'Impressions': 'sum', 'Url Clicks': 'sum', 'Average Position': 'mean'}).reset_index()
@@ -56,7 +64,10 @@ if st.button('Run'):
         result_df = find_pages_to_merge(grouped_data, position_threshold)
 
         if not result_df.empty:
-            st.write(result_df)
+            st.markdown('### Pages To Merge')
+            st.dataframe(result_df)
+            total_pages_to_merge = result_df['Number of Pages to Merge'].sum()
+            st.markdown(f'#### Total Number of Pages to Merge: {total_pages_to_merge}')
             csv = result_df.to_csv(index=False).encode()
             st.download_button(
                 label="Download as CSV",
