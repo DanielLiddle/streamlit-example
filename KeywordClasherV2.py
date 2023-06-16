408613afcb1ee89cd6bac3d1b5fb3f9f6dc02dc9
DanielLiddle <48800605+DanielLiddle@users.noreply.github.com>
2023-06-16 15:45:59 +0100

    Update KeywordClasherV2.py

--- KeywordClasherV2.py
+++ KeywordClasherV2.py
@@ -5,6 +5,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import Flow
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
+from datetime import datetime
 
 
 def get_authorization_url(client_id, client_secret):
@@ -78,4 +79,61 @@ client_secret = st.text_input("Client Secret", type="password")
 if client_id and client_secret:
     auth_url, flow = get_authorization_url(client_id, client_secret)
 
-    if st.button
+    if st.button('Authorize Access'):
+        st.markdown(f"[Authorize Access]({auth_url})")
+
+    authorization_response = st.text_input("Enter the authorization code here:")
+
+    if authorization_response:
+        credentials = exchange_code_for_credentials(flow, authorization_response)
+        searchconsole = build('webmasters', 'v3', credentials=credentials)
+        sites = searchconsole.sites().list().execute()
+
+        site_url = st.selectbox('Select your site:', [site['siteUrl'] for site in sites['siteEntry']])
+        start_date = st.date_input("Start date")
+        end_date = st.date_input("End date")
+
+        min_impressions = st.number_input('Enter Minimum Impressions:', min_value=1, value=500)
+        position_threshold = st.number_input('Enter Average Position Threshold:', min_value=1, value=20)
+
+        if st.button('Run Analysis'):
+            response = searchconsole.searchanalytics().query(
+                siteUrl=site_url,
+                body={
+                    'startDate': start_date.strftime('%Y-%m-%d'),
+                    'endDate': end_date.strftime('%Y-%m-%d'),
+                    'dimensions': ['query', 'page'],
+                    'rowLimit': 10000
+                }
+            ).execute()
+
+            rows = response['rows']
+            data = pd.DataFrame([{
+                'Query': row['keys'][0],
+                'Landing Page': row['keys'][1],
+                'Impressions': row['impressions'],
+                'Url Clicks': row['clicks'],
+                'Average Position': row['position']
+            } for row in rows])
+
+            st.markdown('### Data Fetched From Google Search Console')
+            st.dataframe(data)
+
+            result_df = find_pages_to_merge(data, position_threshold)
+
+            if not result_df.empty:
+                st.markdown('### Pages To Merge')
+                st.dataframe(result_df)
+                total_pages_to_merge = result_df['Number of Pages to Merge'].sum()
+                st.markdown(f'#### Total Number of Pages to Merge: {total_pages_to_merge}')
+                csv = result_df.to_csv(index=False).encode()
+                st.download_button(
+                    label="Download as CSV",
+                    data=csv,
+                    file_name='pages_to_merge.csv',
+                    mime='text/csv'
+                )
+            else:
+                st.write('No pages to merge found with the chosen parameters.')
+else:
+    st.warning('Please enter your Google Cloud Client ID and Client Secret.')
